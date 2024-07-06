66f036a6cdfe0492516d6a4119b542d849f68027
Ag18231766 <amartyagupta18231766@gmail.com>
2024-07-06 23:34:49 +0530

    Adding cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import RootRouter from './routes';
-
+import cors from 'cors'
 import { prototype } from 'events';
 
 const app = express();
 
 
 app.use("/api/v1",RootRouter);
+app.use(cors)
 
 app.get('/',(req,res) => {
     res.json({
