4e7ab2bad0ae7dc3fbb9bfcdb83ac130eb940602
Ag18231766 <amartyagupta18231766@gmail.com>
2024-07-07 10:05:28 +0530

    SomeChangesInPackage.json

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use("/api/v1", routes_1.default);
+app.use(cors_1.default);
 app.get('/', (req, res) => {
     res.json({
         message: "hello from index.ts"
