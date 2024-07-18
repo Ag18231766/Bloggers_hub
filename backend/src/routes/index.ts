42af015287a05c123aa7aafe61ee8b66eafc5853
Ag18231766 <amartyagupta18231766@gmail.com>
2024-07-18 21:16:25 +0530

    ..

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import UserRouter from './users';
 import PostsRouter from './posts';
 import TagRouter from './tags';
-
+import cors from 'cors';
 
 const RootRouter = express.Router();
 
@@ -10,7 +10,7 @@ const RootRouter = express.Router();
 RootRouter.use("/users",UserRouter);
 RootRouter.use("/posts",PostsRouter);
 RootRouter.use("/tags",TagRouter);
-
+RootRouter.use(cors());
 
 
 
