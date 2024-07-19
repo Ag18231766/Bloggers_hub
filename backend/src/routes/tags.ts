409b4b4a44285de68e7dad040aa75ad9e3b9937d
Ag18231766 <amartyagupta18231766@gmail.com>
2024-07-19 15:22:36 +0530

    AddingTypes

--- backend/src/routes/tags.ts
+++ backend/src/routes/tags.ts
@@ -102,7 +102,7 @@ TagRouter.post("/",authMiddleware,async (req:CustomRequest,res:Response) => {
 
 TagRouter.get('/tag',authMiddleware,async (req:Request,res:Response) => {
     try{
-        const tags = prisma.tags.findMany({
+        const tags:{tag:string}[] = await prisma.tags.findMany({
             select:{
                 tag:true
             }
